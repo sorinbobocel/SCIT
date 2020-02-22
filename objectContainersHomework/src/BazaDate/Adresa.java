dc11850be946825b5720e23b2264a1c2ee4c8d3b
Sorin <56653432+sorinbobocel@users.noreply.github.com>
2020-02-22 20:59:02 +0200

    Tema 5
    Containers.

---
+++ objectContainersHomework/src/BazaDate/Adresa.java
@@ -0,0 +1,45 @@
+package BazaDate;
+
+public class Adresa {
+    private String oras;
+    private String strada;
+    private int nr;
+
+    public Adresa(String oras, String strada, int nr) {
+        this.oras = oras;
+        this.strada = strada;
+        this.nr = nr;
+    }
+
+    public String getOras() {
+        return oras;
+    }
+
+    public void setOras(String oras) {
+        this.oras = oras;
+    }
+
+    public String getStrada() {
+        return strada;
+    }
+
+    public void setStrada(String strada) {
+        this.strada = strada;
+    }
+
+    public int getNr() {
+        return nr;
+    }
+
+    public void setNr(int nr) {
+        this.nr = nr;
+    }
+
+    @Override
+    public String toString() {
+        return "Adresa:" +
+                "Oras " + oras + '\'' +
+                ", Strada '" + strada + '\'' +
+                ", nr. " + nr;
+    }
+}
---
+++ objectContainersHomework/src/Persoane/Angajat.java
@@ -0,0 +1,14 @@
+package Persoane;
+
+import Persoane.Persoana;
+
+public class Angajat extends Persoana {
+
+    public Angajat(String name, int age) {
+        super(name, age);
+    }
+
+    public Angajat(String name) {
+        super(name);
+    }
+}
